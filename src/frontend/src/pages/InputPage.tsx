5dbe9c3393c9e922d351287d4efaf6f060ad1a45
Rue Ire <reerine20@msn.com>
2025-10-26 09:40:46 +0200

    update InputPage.tsx
    - form made, takes input.
    - handleSubmit moved from button to form
    -value changes using useState. Change to handlechange later?

--- src/frontend/src/pages/InputPage.tsx
+++ src/frontend/src/pages/InputPage.tsx
@@ -5,10 +5,18 @@ import { type ProjectInput, ProjectInputSchema, type ProjectOutput } from "@myor
 import React from "react";
 import Loader from "../components/Loader";
 import PlaceHolderOutput from "./OutputPage";
+import '../inputPage.css';
 
 const PlaceHolderInput = ({ input }: { input: ProjectInput }) => {
   const [loading, setLoading] = React.useState(false);
   const [showOutput, setShowOutput] = React.useState(false);
+  //input states, create handlechange?
+  const [applicantID, setApplicantID] = React.useState("");
+  const [memberID, setMemberID] = React.useState("");
+  const [budget, setBudget] = React.useState("");
+  const [grant, setGrant] = React.useState("");
+  const [desc, setDesc] = React.useState("");
+
 
   const placeHolderOutput: ProjectOutput = {
     success: {
@@ -54,29 +62,62 @@ const PlaceHolderInput = ({ input }: { input: ProjectInput }) => {
 
   // UI here is very basic, just for demonstration purposes
   return (
-    <div>
-      <h2>Project Input</h2>
-      <p>
-        <strong>Lead Applicant Business ID:</strong>{" "}
-        {input.consortium.leadApplicantBusinessId}
-      </p>
-      <p>
-        <strong>Member Business IDs:</strong>{" "}
-        {input.consortium.memberBusinessIds.join(", ")}
-      </p>
-      <p>
-        <strong>Project Budget:</strong> {input.project.budget}
-      </p>
-      <p>
-        <strong>Requested Grant:</strong> {input.project.requestedGrant}
-      </p>
-      <p>
-        <strong>Project Description:</strong> {input.project.description}
-      </p>
-      <h3>Is input data valid?</h3>
-      <p>{ProjectInputSchema.safeParse(input).success.toString()}</p>
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <h2>Project Input</h2>
+        <div className="inputs-grid">
+          <div className="input-box">
+            <input onChange={(e) => setApplicantID(e.target.value)}
+              type="text"
+              id="applicantID-input"
+              name="applicant-business-id"
+            //value = {input.consortium.leadApplicantBusinessId}
+            //input is in form 010823-3
+              value={applicantID}
+              placeholder="Lead Applicant Business ID" />
+          </div>
+          <div className="input-box">
+            <input onChange={(e) => setMemberID(e.target.value)} 
+              type="text"
+              id="memberID-input"
+              name="member-business-id"
+            //value = {input.consortium.memberBusinessIds.join(", ")}
+              value={memberID}
+              placeholder="Member Business IDs" />
+          </div>
+          <div className="input-box">
+            <input onChange={(e) => setBudget(e.target.value)} 
+              id="budget-input"
+              type="text"
+              name="project-budget"
+            // value = {input.project.budget}
+              value={budget}
+              placeholder="Project Budget" />
+          </div>
+          <div className="input-box">
+            <input onChange={(e) => setGrant(e.target.value)} 
+              type="text"
+              id="grant-input"
+              name="requested-grant"
+            // value = {input.project.requestedGrant}
+              value={grant}
+              placeholder="Requested Grant" />
+          </div>
+          <div className="input-box">
+            <input onChange={(e) => setDesc(e.target.value)} 
+              type="text"
+              id="desc-input"
+              name="project-desc"
+            //value = {input.project.description}
+              value={desc}
+              placeholder="Project Description" />
+          </div>
+        </div>
+        <h3>Is input data valid?</h3>
+        <p>{ProjectInputSchema.safeParse(input).success.toString()}</p>
 
-      <button onClick={handleSubmit}>Submit</button>
+      <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
