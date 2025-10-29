af7f827baeb8158c7aa4ca8fb9b07ec31e2504cd
Simo <simo.alanne@tuni.fi>
2025-10-29 14:31:20 +0200

    Add helper function to frontend that can be used to parse the financial stats copy pasted from kauppalehti.fi

---
+++ src/frontend/src/utils/kauppalehtiParser.ts
@@ -0,0 +1,29 @@
+/**
+ * This function can be used to parse financial statistics user pasted from kauppalehti.fi
+ * @param copyPastedFinancialStats - The raw text copied from kauppalehti.fi
+ * @returns An object containing arrays of revenues and profits for the last 5 years.
+ */
+const parseKauppalehtiData = (copyPastedFinancialStats: string) => {
+  const rows = copyPastedFinancialStats
+    .split("\n")
+    .map((row) => row.replaceAll(" ", "").toLowerCase());
+
+  const getFiveAfter = (previousRowKey: "liikevaihto" | "liiketulos") => {
+    const keyIndex = rows.findIndex((row) => row.startsWith(previousRowKey));
+    const values = rows.slice(keyIndex + 1, keyIndex + 6);
+    // if the number is large then it will contain "milj" at the end
+    return values.map((val) => {
+      if (val.endsWith("milj")) {
+        return parseFloat(val.replace("milj", "")) * 1_000_000;
+      }
+      return parseFloat(val);
+    });
+  };
+
+  return {
+    revenues: getFiveAfter("liikevaihto"),
+    profits: getFiveAfter("liiketulos"),
+  };
+};
+
+export default parseKauppalehtiData;
